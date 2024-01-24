bf893e71f20a98cbf2b32d58bdb3632e36b116dd
smath <smathewslynch@gmail.com>
2024-01-24 11:53:41 +0000

    Added flexable growth factor. Added validation to growth factor

--- src/main/java/GItHubEx/ArrListEx.java
+++ src/main/java/GItHubEx/ArrListEx.java
@@ -2,11 +2,35 @@ package GItHubEx;
 
 
 public class ArrListEx {
+    private static final int MAX_EXPANSION = 10000;
+    private static final int MAX_INITIAL_CAPACITY = 100;
    private int[] data;
+   private int capacity = 10;
    private int num_elements;
+   private int expanstionFactor = 10;
 
-    public ArrListEx() {
-        data = new int[10];
+    public ArrListEx(int capacity) {
+        if (capacity < 0 || capacity > MAX_EXPANSION){
+            capacity = 10;
+        }
+        this.capacity = capacity;
+        data = new int[capacity];
+    }
+
+    public ArrListEx(int capacity, int expanstionFactor) {
+        if (capacity <= MAX_INITIAL_CAPACITY && capacity > 0){
+            this.capacity = capacity;
+        } else if (capacity > MAX_INITIAL_CAPACITY) {
+            this.capacity = MAX_INITIAL_CAPACITY;
+        }
+
+        data = new int[capacity];
+
+        if (this.expanstionFactor <= MAX_EXPANSION && expanstionFactor > 0){
+            this.expanstionFactor = expanstionFactor;
+        }else if(expanstionFactor > MAX_EXPANSION) {
+            this.expanstionFactor = MAX_EXPANSION;
+        }
     }
 
     public int size(){
